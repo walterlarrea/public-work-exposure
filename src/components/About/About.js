3359deeca070160314eb8147ac383baf33192aa8
Walter Larrea <walterlarrea@live.com>
2023-05-24 21:39:18 -0300

    change text about me

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -6,7 +6,8 @@ const About = ({ id }) => (
     <StyledSectionHr />
 
     <p>
-      Hello! I'm Walter, and I'm based in Uruguay, looking forward to landing a job in the IT industry.
+      Hello! I'm Walter, and I'm based in Uruguay, looking forward to work in the IT industry.
+      I am eager to develop software and make the difference.
     </p>
     <p>
       After typing my first lines of code at the age of sixteen, I fell in love with programming and have wanted to work in coding ever since.
@@ -15,24 +16,9 @@ const About = ({ id }) => (
       When I was nineteen, I was still studying Computer Science in high school and using all my spare time trying to code anything I could.
       I created simple C programs, some VB.NET school assignments, and even did some 'freelance' work creating little .LUA scripts for games.
     </p>
-    {/* <p>
-      However, I then became confused about studying and personal stuff, so I decided to get a job and start living independently.
-      A couple of years later, I found myself programming again while finishing high school and exploring new opportunities.
-    </p> */}
     <p>
       These days, I spend most of my free time studying, practicing, and gaining hands-on experience.
       I'm taking every opportunity to improve my technical and soft skills, while also learning about the software development processes.
-      {/* Whenever I incorporate new knowledge into my work or deliver a Git push to my repositories, I feel a rush of motivation. */}
-    </p>
-    <p>
-      Recently, I completed a Front-end Web development course that covered vanilla JS, HTML, and CSS.
-      From there, I took the time to understand the key features of JavaScript, and dove into React, Node, Express, and MongoDB.
-      So my focus right now is on the MERN stack.
-    </p>
-    <p>
-      Overall, I am excited to continue learning and growing as a developer,
-      and I am eager to contribute my skills and knowledge to a team or organization that values commitment,
-      collaboration, and continuous improvement.
     </p>
     <br />
     <br />
